ea3938e6081bdfe9674ff8828bf81fa9e63fe864
Nadeem740 <nadeemblayparambil@gmail.com>
2023-06-13 22:23:05 +0530

    a

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 from flask import Flask, request
-from imageconvert import finalFunct
+# from imageconvert import finalFunct
 app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def helloworld():
    return "hello"
 
-@app.route('/convert-text',methods=['GET'])
-def hello_world():
-   print(request.args.get("name"))
-   a=finalFunct(request.args.get("name"))
-   return a
+# @app.route('/convert-text',methods=['GET'])
+# def hello_world():
+#    print(request.args.get("name"))
+#    a=finalFunct(request.args.get("name"))
+#    return a
 
 if __name__ == '__main__':
    app.run()
